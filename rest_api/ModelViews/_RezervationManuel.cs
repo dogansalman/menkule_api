8e8b6312f1b59935a570121640988c59f7043986
dogansalman <dogan.salman@yahoo.com>
2018-04-20 17:57:29 +0300

    manuel rezervations

---
+++ rest_api/ModelViews/_RezervationManuel.cs
@@ -0,0 +1,22 @@
+﻿using rest_api.Models;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace rest_api.ModelViews
+{
+    public class _RezervationManuel
+    {
+        [Required]
+        public ICollection<RezervationVisitors> visitors { get; set; }
+        [Required]
+        public DateTime checkin { get; set; }
+        [Required]
+        public DateTime checkout { get; set; }
+        [Required]
+        public int advert_id { get; set; }
+
+    }
+}
